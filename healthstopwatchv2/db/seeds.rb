5cd69b6dea27cc829eb70493afa443bae8e3705c
brenbread <santos.brenj@gmail.com>
2016-11-29 14:28:16 -0800

    ACTUAL Initial Commit
    Created Pages for Medicines and Stopwatches. Also has basic Bootstrap
    Sass theme that needs some work.  Actual countdown will be worked on
    soon.
    
    Bootstrap Sass Components: http://getbootstrap.com/components

---
+++ healthstopwatchv2/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Medicine.create(Name: 'Med_A', time: 60)
+Medicine.create(Name: 'Med_B', time: 30)
+Medicine.create(Name: 'Med_C', time: 90)
